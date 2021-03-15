167595e781d50e69acc6565749d03a8fc8284377
Chris Gantt <gantt.art@gmail.com>
2021-03-15 13:13:14 -1000

    Created header and footer components, and imported and invoked them in App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/header.js';
+import Footer from './components/footer.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Header />
+        <Footer />
       </div>
     );
   }
