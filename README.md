ff5627be2436d0a41e124aa684f7deccd0c97074
Chris Gantt <gantt.art@gmail.com>
2021-03-16 16:16:52 -1000

    Feature 2 completed: Allow users to vote for their favorite beast.

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ A React application that displays images and information of horned animals. This
 
 ## Time Estimates
 
-- Lab 2: Display images from JSON
+- Lab 2, Feature 1: Display images from JSON
 - Estimate of time needed to complete: 20min
 - Start time: 3:11pm
 - Finish time: 3:16pm
 - Actual time needed to complete: 5min
+
+- Lab 2, Feature 2: Allow users to vote for their favorite beast
+- Estimate of time needed to complete: 30min
+- Start time: 3:55pm
+- Finish time: 4:15pm
+- Actual time needed to complete: 20min
--- src/components/hornedBeasts.js
+++ src/components/hornedBeasts.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    };
+  }
+
+  addFavorite = () => {
+    this.setState({favorites: this.state.favorites + 1});
+  }
+
   render() {
     return (
-      <div class="horned-beast">
+      <div className="horned-beast">
         <h2>{this.props.title}</h2>
-        <img src={this.props.source} alt={this.props.description}/>
+        <img onClick={this.addFavorite} src={this.props.source} alt={this.props.description}/>
         <p>{this.props.description}</p>
+        <div>❤️ {this.state.favorites}</div>
       </div>
     );
   }
