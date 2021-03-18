393bfcfbb57d25407d399aa6429d89f05f70c321
Chris Gantt <gantt.art@gmail.com>
2021-03-17 14:35:07 -1000

    Refactored data.json import to import in app and pass that data into main.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './components/header.js';
 import Main from './components/main.js';
 import Footer from './components/footer.js';
+import rawData from './data.json';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Main />
+        <Main data={rawData} />
         <Footer />
       </div>
     );
--- src/components/main.js
+++ src/components/main.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import HornedBeast from './hornedBeasts.js';
-import data from '../data.json';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
   render() {
     return (
       <main>
         <Container>
           <Row>
-            {data.map((beast) => (
+            {this.props.data.map((beast) => (
               <div key={beast.title}>
                 <HornedBeast
                   title={beast.title}
