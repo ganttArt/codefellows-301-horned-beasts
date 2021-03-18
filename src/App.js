9f4d1dfc78a37de329e2670de7cd4f5e886946e0
Chris Gantt <gantt.art@gmail.com>
2021-03-17 15:26:46 -1000

    Created a modal that is invoked in app.js.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import Header from './components/header.js';
 import Main from './components/main.js';
 import Footer from './components/footer.js';
+import SelectedBeast from './components/selectedBeast';
 import rawData from './data.json';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedBeast: rawData[7]
+    }
+  }
+
+  updateSelectedBeast = (beast) => {
+    this.setState({ selectedBeast: beast })
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Main data={rawData} />
+        <SelectedBeast beast={this.state.selectedBeast}/>
+        <Main
+          data={rawData}
+          updateSelectedBeast={this.updateSelectedBeast}
+        />
         <Footer />
       </div>
     );
--- src/components/main.js
+++ src/components/main.js
@@ -7,7 +7,7 @@ class Main extends React.Component {
   constructor(props) {
     super(props)
   }
-  
+
   render() {
     return (
       <main>
